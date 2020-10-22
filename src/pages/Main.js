e769e598b341dc935dd7dfbae7ae4b07a595adfd
Arnold <arnolddonpaul@gmail.com>
2020-10-22 10:45:13 -0400

    tablet view implemented

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -11,61 +11,80 @@ class Main extends Component {
         return (
             <>
                 <nav className="navBar">
-                    <h1 className="navBar__header">joanna.to</h1>
-                    {
-                        this.state.infoExpanded === false && (
-                            <h2 className="navBar__info" onClick={() => this.setState({ infoExpanded: true })}>information +</h2>
-                        )
-                    }
+                    <div className="navBar__infoInitial">
+                        {
+                            this.state.infoExpanded === false && (
+                                <>
+                                    <h1 className="navBar__header">joanna.to</h1>
+                                    <h2 className="navBar__info" onClick={() => this.setState({ infoExpanded: true })}>information +</h2>
+                                </>
+                            )
+                        }
+                    </div>
                     {
                         this.state.infoExpanded === true && (
-                            <div>
-                                <h2 className="navBar__info" onClick={() => this.setState({ infoExpanded: false })}>information −</h2>
-                                <p className="navBar__infoExpanded">i'm a graphic designer based in toronto, canada.</p>
-                                <p className="navBar__infoExpanded">* i’m currently an intermediate designer at oliver & bonacini hospitality</p>
-                                <div className="contactBox">
-                                    <div className="contactBox__direct">
-                                        <p>hello@joanna.to</p>
-                                        <p>1 647 961 0759</p>
-                                    </div>
-                                    <div className="contactBox__social">
-                                        <p>linkedin</p>
-                                        <p>instagram</p>
+                            <>
+                                <div className="navBar__infoInitial">
+                                    <h1 className="navBar__header">joanna.to</h1>
+                                    <h2 className="navBar__info" onClick={() => this.setState({ infoExpanded: false })}>information −</h2>
+                                </div>
+                                <div>
+                                    <p className="navBar__infoExpanded">i'm a graphic designer based in toronto, canada.</p>
+                                    <p className="navBar__infoExpanded">* i’m currently an intermediate designer at oliver & bonacini hospitality</p>
+                                    <div className="contactBox">
+                                        <div className="contactBox__direct">
+                                            <p>hello@joanna.to</p>
+                                            <p>1 647 961 0759</p>
+                                        </div>
+                                        <div className="contactBox__social">
+                                            <p>linkedin</p>
+                                            <p>instagram</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </>
                         )
                     }
                 </nav>
 
                 <section>
                     <div className="projectBox">
-                        <h1 className="projectBox__title">arcadian</h1>
-                        <h2 className="projectBox__subtitle">menu / art direction</h2>
+                        <div className="projectBox__textBox">
+                            <h1 className="projectBox__title">arcadian</h1>
+                            <h2 className="projectBox__subtitle">menu / art direction</h2>
+                        </div>
                         <img className="projectBox__image" src={nicCage} alt="arcadian"></img>
                     </div>
 
                     <div className="projectBox">
-                        <h1 className="projectBox__title">bannock</h1>
-                        <h2 className="projectBox__subtitle">menu / art direction</h2>
+                        <div className="projectBox__textBox">
+                            <h1 className="projectBox__title">bannock</h1>
+                            <h2 className="projectBox__subtitle">menu / art direction</h2>
+                        </div>
                         <img className="projectBox__image" src={nicCage} alt="arcadian"></img>
                     </div>
 
                     <div className="projectBox">
-                        <h1 className="projectBox__title">little foot</h1>
-                        <h2 className="projectBox__subtitle">user experience & interface</h2>
+                        <div className="projectBox__textBox">
+                            <h1 className="projectBox__title">little foot</h1>
+                            <h2 className="projectBox__subtitle">user experience & interface</h2>
+                        </div>
                         <img className="projectBox__image" src={nicCage} alt="arcadian"></img>
                     </div>
 
                     <div className="projectBox">
-                        <h1 className="projectBox__title">buffo</h1>
-                        <h2 className="projectBox__subtitle">menu / typography</h2>
+                        <div className="projectBox__textBox">
+                            <h1 className="projectBox__title">buffo</h1>
+                            <h2 className="projectBox__subtitle">menu / typography</h2>
+                        </div>
                         <img className="projectBox__image" src={nicCage} alt="arcadian"></img>
                     </div>
 
                     <div className="projectBox">
-                        <h1 className="projectBox__title">canteen</h1>
-                        <h2 className="projectBox__subtitle">menu / typography / art direction</h2>
+                        <div className="projectBox__textBox">
+                            <h1 className="projectBox__title">canteen</h1>
+                            <h2 className="projectBox__subtitle">menu / typography / art direction</h2>
+                        </div>
                         <img className="projectBox__image" src={nicCage} alt="arcadian"></img>
                     </div>
 
